6bc4cdd7f4b7465578feb814ae4b4b8298682b7a
Harley Thomas <harleyjackthomas@gmail.com>
2025-03-17 09:15:35 +0700

    Updating deps, and styling.:

--- packages/sdk/src/CredentialsScreen.tsx
+++ packages/sdk/src/CredentialsScreen.tsx
@@ -1,9 +1,17 @@
 import {ScrollView, TopAppBar, View, YStack} from "@truststack/ui";
 import {AddCredentialSheet} from "./AddCredentialSheet";
 import {CredentialsList} from "./CredentialsList";
+
 export function CredentialsScreen(): JSX.Element {
   return (
-    <YStack position="relative" maxHeight="100%" maxWidth="100%" flex={0}>
+    <YStack
+      position="relative"
+      maxHeight="100%"
+      maxWidth="100%"
+      height={"100%"}
+      width={"100%"}
+      flex={0}
+    >
       <TopAppBar size="small" backgroundColor="transparent">
         <TopAppBar.TopRail>
           <TopAppBar.SmallHeadline size="large">
